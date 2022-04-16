57123d0bc339d67f6ae24519abb3c5ca76ea918e
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-04-16 21:53:26 +0100

    Add routes link to App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import React, {Component} from 'react';
-import { Calendar } from '@progress/kendo-react-dateinputs'
-import '@progress/kendo-theme-default/dist/all.css'
 import './App.css';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-        <div className="App">
-          <h1>Calendar</h1>
-          <Calendar/>
-        </div>      
+      <h1>Demo</h1>
+      <Link to="/hello">Hello</Link>
     </div>
   );
 }
