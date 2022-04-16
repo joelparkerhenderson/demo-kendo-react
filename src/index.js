6637995089cd0e4e33a69b76c63637b51523da34
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-04-16 21:53:09 +0100

    Add routes to index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import { render } from 'react-dom';
 import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Hello from './routes/hello';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const rootElement = document.getElementById("root");
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="hello" element={<Hello />} />
+    </Routes>
+  </BrowserRouter>,
+  rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
